1750408d68c4d728091c6e6d50db255105aa6ae8
waner <wanersbh@gmail.com>
2021-07-16 14:41:03 -0300

    01-11 Banco de dados H2, camadas

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -3,6 +3,14 @@ package com.devsuperior.dscatalog.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_category")
 public class Category implements Serializable {
 	
 	//É um padrão da linguagem java para que o objeto possa ser convertido em bytes.
@@ -11,8 +19,14 @@ public class Category implements Serializable {
 	// - Trafegar nas redes;
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	public Category() {
+		
+	}
 	
 	public Category(Long id, String name) {
 		super();
