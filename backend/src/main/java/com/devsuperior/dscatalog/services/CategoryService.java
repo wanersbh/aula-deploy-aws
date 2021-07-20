0dd2d1e49e18089b9680be860090170fdeb8302d
waner <wanersbh@gmail.com>
2021-07-20 09:01:32 -0300

    01-15 Criando um ambiente de execucao no Postman

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -18,8 +20,11 @@ public class CategoryService {
 	//Anotação para envolver toda operação em uma transação
 	//A Propriedade readOnly = true é para evitar um look na base de dados, pq nesse caso somente é uma consulta.
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findAll() {
+
+		List<Category> list = categoryRepository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 	}
 
 }
