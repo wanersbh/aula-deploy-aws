d59af2661e43fac45ff9687356e12f12f6362457
waner <wanersbh@gmail.com>
2021-07-20 09:59:51 -0300

    Inserindo nova categoria com POST

--- backend/src/main/java/com/devsuperior/dscatalog/resourses/CategoryResourse.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resourses/CategoryResourse.java
@@ -1,13 +1,17 @@
 package com.devsuperior.dscatalog.resourses;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.services.CategoryService;
@@ -31,6 +35,16 @@ public class CategoryResourse {
 		CategoryDTO categoryDTO = service.findById(id);
 		
 		return ResponseEntity.ok().body(categoryDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		CategoryDTO categoryDTO = service.insert(dto);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(categoryDTO.getId()).toUri();
+		
+		return ResponseEntity.created(uri).body(categoryDTO);
 	}
 
 }
--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -36,4 +36,14 @@ public class CategoryService {
 		return new CategoryDTO(categoria);
 	}
 
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
+
 }
