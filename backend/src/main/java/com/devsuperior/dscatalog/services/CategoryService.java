2550b63eb2c3139f8f268df00eb1c5a724680426
waner <wanersbh@gmail.com>
2021-07-21 15:06:34 -0300

    01-21 Dados de auditoria

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -6,12 +6,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.services.exceptions.DataBaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -61,6 +64,17 @@ public class CategoryService {
 
 		}catch(EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found "+ id);
+		}
+		
+	}
+
+	public void delete(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found "+ id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DataBaseException("Integrity violation");
 		}
 		
 	}
