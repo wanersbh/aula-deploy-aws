ba2c0f25eb9d9064c31de897d1a5df276a1b4af7
waner <wanersbh@gmail.com>
2021-08-19 07:20:16 -0300

    02-37 Teste de integração para findAllPaged

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,95 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+// A anotação @Transactional irá realizar um rollback para cada teste
+// Desta forma, a sequencia de execução dos testes não importará. 
+// Por exemplo: tenho um teste que deleta um elemente e outro que verifica a quantidade total de elementos
+@Transactional
+@SpringBootTest
+public class ProductServiceIT {
+
+	@Autowired
+	private ProductService service;
+
+	@Autowired
+	private ProductRepository repository;
+
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+
+	}
+
+	@Test
+	public void deleteShouldDeleteResorceWhenIdExists() {
+
+		service.delete(existingId);
+
+		Assertions.assertEquals(countTotalProducts - 1, repository.count());
+	}
+
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+	}
+
+	@Test
+	public void findAllPagedShouldReturnPageWhenPageZeroSizeTen() {
+
+		PageRequest pageRequest = PageRequest.of(0, 10);
+
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+
+		PageRequest pageRequest = PageRequest.of(50, 10);
+
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+	}
+
+}
